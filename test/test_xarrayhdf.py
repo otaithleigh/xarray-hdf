0fa08e5c2a50a517e43f1d5cb4ffdb8c815b7722
Peter Talley <peterctalley@gmail.com>
2021-07-25 07:48:01 -0400

    Add initial tests

--- test/test_xarrayhdf.py
+++ test/test_xarrayhdf.py
@@ -1,1 +1,27 @@
-import xarrayhdf
+import pandas as pd
+import xarray as xr
+
+from xarrayhdf import dataset_to_dataframe
+
+reference_ds = xr.Dataset(
+    data_vars={
+        'disp': range(10),
+        'force': range(10),
+    },
+    coords={
+        'time': range(10),
+    },
+    attrs={
+        'timestamp': 'Mon Jun 28 07:42:09 2021',
+    },
+)
+reference_ds.disp.attrs['units'] = 'm'
+reference_ds.time.attrs['units'] = 's'
+reference_ds.force.attrs['units'] = 'N'
+
+
+def test_converted_attrs():
+    df = dataset_to_dataframe(reference_ds)
+    assert df.attrs['dataset_attrs::disp::units'] == 'm'
+    assert df.attrs['dataset_attrs::time::units'] == 's'
+    assert df.attrs['dataset_attrs::force::units'] == 'N'
